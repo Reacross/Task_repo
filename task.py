b33818ee552dba6a5c257d90f8a478ba22cdbfb8
Ivan <priorat.ceon@gmail.com>
2024-01-21 17:37:50 +0200

    new commit

--- task.py
+++ task.py
@@ -16,3 +16,6 @@ class Task:
         print('{:^5} {:^15} {:^15} {:^10} {:^10}'.format(self.id, self.title, self.description, self.status, self.due_date))
 
 
+if __name__ == '__main__':
+    task_1 = Task(2496, "To-do-do", 'First-to finnish', 'In Process', '25-01-2024')
+    task_2 = Task(2487, "To-do-or-not", 'First-to finish', 'To do', '25-11-2024')
